26957d2082b67d5f115db817f20d0ee9fe3a7f80
kessie2862 <kayp3477@gmail.com>
2025-12-11 17:02:29 +0000

    feat: add three new paths to handle group request flow

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from .views import (
-    CreateSavingsGroupView, MyGroupsListView, GroupDetailView, FullSignupView, SendOTPView, VerifyOTPView,
-    CustomLoginView, ForgotPasswordView, ResetPasswordView, MeView, AllGroupsListView
+    CreateSavingsGroupView, MyGroupsListView, GroupDetailView, FullSignupView, SendOTPView,
+    VerifyOTPView, CustomLoginView, ForgotPasswordView, ResetPasswordView, MeView, AllGroupsListView,
+    GroupJoinRequestView, GroupRequestsListView, GroupRequestActionView
 )
 from rest_framework_simplejwt.views import TokenRefreshView
 from django.urls import path
@@ -15,9 +16,14 @@ urlpatterns = [
     path('reset-password/', ResetPasswordView.as_view(), name='reset_password'),
     path('me/', MeView.as_view(), name='me'),
 
-    # groups endpoints
+    # Groups endpoints
     path('groups/create/', CreateSavingsGroupView.as_view(), name='create-savings-group'),
     path('groups/my-groups/', MyGroupsListView.as_view(), name='my-groups'),
     path('groups/all/', AllGroupsListView.as_view(), name='all-groups'),
     path('groups/<int:id>/', GroupDetailView.as_view(), name='group-detail'),
+
+    # Join group request endpoints
+    path('groups/<int:group_id>/request_join/', GroupJoinRequestView.as_view(), name='group-request-join'),
+    path('groups/<int:group_id>/requests/', GroupRequestsListView.as_view(), name='group-requests-list'),
+    path('groups/requests/<int:pk>/action/', GroupRequestActionView.as_view(), name='group-request-action'),
 ]
