35e8a68b8f8812e1c49ccc47ece105e82968e848
kessie2862 <kayp3477@gmail.com>
2026-03-25 16:09:56 +0000

    add build_files.sh for Vercel

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+echo "=== System Check ==="
+python3.12 --version
+ls -la
+
 echo "=== Running collectstatic ==="
-python manage.py collectstatic --noinput --verbosity=0
+python3.12 manage.py collectstatic --noinput --verbosity=0
 
 echo "=== Running migrations ==="
-python manage.py migrate --noinput
+python3.12 manage.py migrate --noinput
+
+echo "=== Build Finished ==="
